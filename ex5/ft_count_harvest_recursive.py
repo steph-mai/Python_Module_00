2c540a35486c95a160f465a938fc07a6ab3b12f3
Stephanie Maire <stmaire@z1r9p1.42lyon.fr>
2026-01-05 15:02:11 +0100

    ex4 and ex5

---
+++ ex5/ft_count_harvest_recursive.py
@@ -0,0 +1,11 @@
+def ft_count_harvest_recursive():
+
+    count = int(input("Days until harvest: "))
+
+    def ft_recursive(current_day, count):
+        if current_day > count:
+            print("Harvest time!")
+            return
+        print(f"Day {current_day}")
+        ft_recursive(current_day + 1, count)
+    ft_recursive(1, count)
