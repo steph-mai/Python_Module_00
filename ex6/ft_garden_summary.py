e9679df7aec5587dec68283e46a03d4e002bcf95
Stephanie Maire <stmaire@z1r10p1.42lyon.fr>
2026-02-06 12:18:33 +0100

    6

--- ex6/ft_garden_summary.py
+++ ex6/ft_garden_summary.py
@@ -1,8 +1,6 @@
-#!/usr/bin/env python3
-
-def ft_garden_summary():
-    name = input("Enter garden name: ")
-    plants_number = input("Enter number of plants: ")
-    print(f"Garden : {name}")
+def ft_garden_summary() -> None:
+    name: str = input("Enter garden name: ")
+    plants_number: int = int(input("Enter number of plants: "))
+    print(f"Garden: {name}")
     print(f"Plants: {plants_number}")
     print("Status: Growing well!")
