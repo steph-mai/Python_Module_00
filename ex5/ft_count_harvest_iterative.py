6d61834472bf94c4d8cf954226b1cc86148aecda
Stephanie Maire <stmaire@z1r10p1.42lyon.fr>
2026-02-06 12:05:57 +0100

    ex5

--- ex5/ft_count_harvest_iterative.py
+++ ex5/ft_count_harvest_iterative.py
@@ -1,7 +1,5 @@
-#!/usr/bin/env python3
-
-def ft_count_harvest_iterative():
-    count = int(input("Days until harvest: "))
+def ft_count_harvest_iterative() -> None:
+    count: int = int(input("Days until harvest: "))
     for i in range(1, count + 1):
         print(f"Day {i}")
     print("Harvest time!")
--- ex5/ft_count_harvest_recursive.py
+++ ex5/ft_count_harvest_recursive.py
@@ -1,13 +1,11 @@
-#!/usr/bin/env python3
+def ft_count_harvest_recursive() -> None:
 
-def ft_count_harvest_recursive():
+    count: int = int(input("Days until harvest: "))
 
-    count = int(input("Days until harvest: "))
-
-    def ft_recursive(current_day, count):
-        if current_day > count:
+    def ft_recursive(current_day: int, max_days: int) -> None:
+        if current_day > max_days:
             print("Harvest time!")
             return
         print(f"Day {current_day}")
-        ft_recursive(current_day + 1, count)
+        ft_recursive(current_day + 1, max_days)
     ft_recursive(1, count)
